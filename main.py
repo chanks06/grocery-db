df23336bdd8b8a5a86a1ec3351a5d12348611bab
Charles <cw.hanks@proton.me>
2024-02-22 11:19:12 -0800

    datatype casting troubleshooting

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ this py script is the main program for pulling data from my on-prem mySQL databa
 import mysql.connector
 import pandas as pd 
 import numpy as np 
+import datetime as dt
 
 #import credentials for connecting to db from config.py file
 from config import HOST, DATABASE, USERNAME, PASSWORD
@@ -31,6 +32,60 @@ def open_connection():
         print(f"Error: {err}")
 
 
+## kk next I want to build a function that receives an SQL query and produces a formatted pandas dataframe: 
+
+def query_to_dataframe(cursor): 
+
+    ### 1. TABLE SELECTION 
+    #first show user what tables are inside the GROCERY database 
+    cursor.execute("SHOW TABLES")
+
+    #list comprehension to grab only name of table, otherwise presenting a list of tuples
+    tables = [table[0] for table in cursor.fetchall()] 
+    print(tables)
+
+    user_query = input("Which table would you like? ")
+
+    while user_query not in tables: 
+        print("Please enter a table from GROCERY DB")
+        user_query = input("Which table would you like? ").lower()
+
+    ### 2. TABLE LOAD
+        
+    cursor.execute(f'SELECT * FROM {user_query}')
+    selected_table = cursor.fetchall() 
+
+    # take contents of cursor (a list of tuples) and contruct a pd dataframe. 
+    # columns arg takes the first element from description, which is the name of the table column from mySQL
+
+    datatypes = {2: int, 253: str, 246: float, 10: dt.datetime }
+
+    relevant_data = [(t[0],t[1]) for t in cursor.description]
+    relevant_dict = {key:value for key,value in relevant_data}
+
+    for key, value in relevant_dict.items(): 
+        if value in datatypes: 
+            relevant_dict[key] = datatypes[value]
+
+
+    df = pd.DataFrame(selected_table, columns = [i[0] for i in cursor.description])
+
+    df.astype(dtype = relevant_dict)
+
+    print(df.dtypes)
+
+   
+
+
+ 
+
+    
+
+    
+
+
+
+
 def close_connection(connection, cursor): 
     if cursor: 
         cursor.close()
@@ -43,6 +98,7 @@ def main():
     connection, cursor = open_connection()
 
     # ETL goes here 
+    query_to_dataframe(cursor)
 
     close_connection(connection, cursor)
 
